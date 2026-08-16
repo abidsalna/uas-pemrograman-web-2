d3c166dc2f75d0503c2f42e83a17f8d5e6136fdc
abidsalna <abidsalna@outlook.com>
2026-08-16 12:59:16 +0800

    Add files via upload

---
+++ backend/models/Loan.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const loanSchema = new mongoose.Schema(
+{
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book",
+        required: true
+    },
+
+    member: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Member",
+        required: true
+    },
+
+    borrowDate: {
+        type: Date,
+        default: Date.now
+    },
+
+    returnDate: {
+        type: Date
+    },
+
+    status: {
+        type: String,
+        enum: ["Dipinjam", "Dikembalikan"],
+        default: "Dipinjam"
+    }
+
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model("Loan", loanSchema);
---
+++ backend/models/book.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+{
+    title: {
+        type: String,
+        required: true
+    },
+
+    author: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    stock: {
+        type: Number,
+        required: true
+    }
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model("Book", bookSchema);
